14844d8c2136745f763ed9d985f32db480520378
Геракл Афанасов <afanasovgerakl@gmail.com>
2024-05-25 16:35:13 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,15 +1,18 @@
 function showDate() {
     let out = document.getElementById('current-date');
     let today = new Date();
+    const weekDays = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 
-    out.innerHTML = `
-        <div class="box">
-            <div>Дата и время Россия: ${today.toLocaleString('ru-RU')}</div>
-            <div>Дата и время Греция: ${today.toLocaleString('el-GR')}</div>
-            <div>Дата и время Зимбабве: ${today.toLocaleString('en-ZW')}</div>
-            <div>Дата и время Панама: ${today.toLocaleString('es-PA')}</div>
-            <div>Дата и время Эстония: ${today.toLocaleString('et-EE')}</div>
-            <div>Дата и время Бельгия: ${today.toLocaleString('fr-BE')}</div>
-        </div>
-    `;
+
+    out.innerHTML += `<div>Россия: ${today.toLocaleString('ru-RU')}</div>`;
+    out.innerHTML += `<div>Греция: ${today.toLocaleString('el-GR')}</div>`;
+    out.innerHTML += `<div>Зимбабве: ${today.toLocaleString('en-ZW')}</div>`;
+    out.innerHTML += `<div>Панама: ${today.toLocaleString('es-PA')}</div>`;
+    out.innerHTML += `<div>Эстония: ${today.toLocaleString('et-EE')}</div>`;
+    out.innerHTML += `<div>Бельгия: ${today.toLocaleString('fr-BE')}</div>`;
+
+    out.innerHTML += `<div>Год: ${today.getFullYear()}</div>`;
+    out.innerHTML += `<div>Месяц: ${today.getMonth()}</div>`;
+    out.innerHTML += `<div>Дата: ${today.getDate()}</div>`;
+    out.innerHTML += `<div>День: ${weekDays[today.getDay()]}</div>`;
 }
